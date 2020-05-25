59e3964cc60841b267a07e0f92661345f2018f49
Luis Filipe F. de Moraes <luisfilipefdm@gmail.com>
2020-05-25 17:18:25 -0400

    TRANSFER COMMIT FOR Custom Gravity
    This commit is the initial and final commit transfering this project from another repo for organization purposes.

---
+++ CCTutorials/Assets/CustomGravity/Scripts/RigidBody_CustomGravityExtension.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidBody_CustomGravityExtension : MonoBehaviour
+{
+    [SerializeField, Range(0.00000001f, 0.001f)]
+    private float sleepSqrVelocityThreshold = 0.0001f;
+
+    private Rigidbody body;
+    private float floatDelay;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        body.useGravity = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if(body.IsSleeping())
+        {
+            GetComponent<Renderer>().material.SetColor("_Color", Color.grey);
+            floatDelay = 0f;
+            return;
+        }
+
+        if(body.velocity.sqrMagnitude < sleepSqrVelocityThreshold)
+        {
+            GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
+            floatDelay += Time.deltaTime;
+            if(floatDelay >= 1f)
+            {
+                return;
+            }
+        }
+        else
+        {
+            floatDelay = 0f;
+        }
+
+        GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        body.AddForce(CustomGravity.GetGravity(body.position), ForceMode.Acceleration);
+    }
+}
