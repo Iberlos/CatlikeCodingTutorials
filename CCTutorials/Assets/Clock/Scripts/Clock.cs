b044d3a24ad84184007e71b4767670d861c3f21c
Luis Filipe F. de Moraes <luisfilipefdm@gmail.com>
2020-05-28 11:36:17 -0400

    TRANSFER COMMIT FOR Clock
    This commit is the initial and final commit transfering this project from another repo for organization purposes.

---
+++ CCTutorials/Assets/Clock/Scripts/Clock.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public Transform hoursTransform, minutesTransform, secondsTransform;
+    const float degreesPerHour = 30f,
+                degreesPerMinute = 6f, 
+                degreesPerSecond = 6f;
+    public bool continuous;
+
+    void Awake()
+    {
+        System.DateTime time = System.DateTime.Now;
+        hoursTransform.localRotation = Quaternion.Euler(0f, time.Hour * degreesPerHour,0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, time.Second * degreesPerSecond, 0f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(continuous)
+        {
+            UpdateContinuous();
+        }
+        else
+        {
+            UpdateDiscrete();
+        }
+    }
+
+    void UpdateDiscrete()
+    {
+        System.DateTime time = System.DateTime.Now;
+        secondsTransform.localRotation = Quaternion.Euler(0f, time.Second * degreesPerSecond, 0f);
+        if (time.Second == 0)
+        {
+            minutesTransform.localRotation = Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+            if (time.Minute == 0)
+            {
+                hoursTransform.localRotation = Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+            }
+        }
+    }
+
+    void UpdateContinuous()
+    {
+        System.TimeSpan time = System.DateTime.Now.TimeOfDay;
+        secondsTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalSeconds * degreesPerSecond, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalMinutes * degreesPerMinute, 0f);
+        hoursTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalHours * degreesPerHour, 0f);
+    }
+}
